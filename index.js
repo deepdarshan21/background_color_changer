a0e95b851646d86b5d46be5ea2a423508a52b02c
deepdarshan21 <deepdarshan21@gmail.com>
2020-09-10 19:43:16 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var color;
+function take_input() {
+    color = document.getElementById("input").value;
+    change_color(color);
+}
+
+function random_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    color = "rgb(" + x + "," + y + "," + z + ")";
+    change_color(color);
+}
+
 function change_color(color) {
     document.body.style.backgroundColor = color;
 }
